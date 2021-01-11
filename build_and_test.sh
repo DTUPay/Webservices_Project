1460bdc274e311eefb9e635646802652c2b823b5
Rubatharisan Thirumathyam <Rubatharisan@gmail.com>
2021-01-11 16:14:28 +0100

    Update build_and_test.sh

--- build_and_test.sh
+++ build_and_test.sh
@@ -4,5 +4,8 @@ set -e
 # DTUPay - Build & Test
 ./DTUPay/build_and_test.sh
 
+# payment service - Build and Test
+./payment/build_and_test.sh
+
 # Final end-to-end test
 ./system_tests/build_and_test.sh
