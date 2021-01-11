7d50bced629dd854db8c28a92ee3061b32731a8e
Rubatharisan Thirumathyam <ruth@netcompany.com>
2021-01-11 18:00:13 +0100

    Renamed DTUPay to dtupay, since Docker does not support uppercase

---
+++ dtupay/build_and_test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+# Set service name based on the current directory name
+SERVICE_NAME=$(basename $(pwd))
+
+# Compile and build the application
+mvn package
+
+# Build Docker image
+docker build -t $SERVICE_NAME:build-${BUILD_NUMBER} .
