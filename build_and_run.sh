a13ab97e28177996d07d5b8a4a16e4ce83b5c6c3
Rubatharisan Thirumathyam <Rubatharisan@gmail.com>
2021-01-11 15:48:06 +0100

    CI test flow

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,21 +1,8 @@
 #!/bin/bash
 set -e
 
-pushd DTUPay
-mvn package -Dquarkus.package.type=uber-jar
-# Create a new docker image if necessary.
-# Restarts the container with the new image if necessary
-# The server stays running.
-# To terminate the server run docker-compose down in the
-# code-with-quarkus direcgtory
-docker-compose up -d
-# clean up images
-docker image prune -f 
-popd
+# DTUPay - Build & Test
+./DTUPay/build_and_test.sh
 
-# Give the Web server a chance to finish start up
-sleep 2s 
-
-pushd system_tests
-mvn test
-popd
+# Final end-to-end test
+./system_tests/build_and_test.sh
