583a9ddae3e8bf110a7e7d5d5e099d8fb3f1a720
Rubatharisan Thirumathyam <ruth@netcompany.com>
2021-01-12 22:42:48 +0100

    build args is used now for building Docker images

--- management_service/build_and_test.sh
+++ management_service/build_and_test.sh
@@ -12,4 +12,4 @@ SERVICE_NAME=$(basename $(pwd))
 mvn package
 
 # Build Docker image
-docker build -t $SERVICE_NAME:${BUILD} .
+docker build --build-arg SERVICE_NAME_ARG=$SERVICE_NAME -t $SERVICE_NAME:${BUILD} .
