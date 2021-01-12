af096dd069680425dbfa6d1110132fd5b121c4e7
Rubatharisan Thirumathyam <ruth@netcompany.com>
2021-01-12 22:46:12 +0100

    Modified the integration test script

--- scripts/test_integration.sh
+++ scripts/test_integration.sh
@@ -4,14 +4,14 @@ set -e
 # Check if BUILD is set, if not lets assume its local
 if [ -z ${BUILD+x} ]; then export BUILD="build-local"; else echo "BUILD is set to '$BUILD'"; fi
 
-echo "Get into system_tests directory"
-cd "$(dirname "$0")"
-
 echo "Starting CI environment"
 docker-compose -f docker-compose.ci.yaml up -d 
 
 echo "Sleep 6 seconds before running tests on CI environment"
 sleep 6
+
+echo "Get into system_tests directory"
+cd system_tests
 
 echo "Executing tests"
 mvn test
