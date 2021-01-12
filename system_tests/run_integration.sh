88fbdc2c115edf2b979a3363483165180e83f56b
Rubatharisan Thirumathyam <Rubatharisan@gmail.com>
2021-01-12 13:49:52 +0100

    Support building locally

--- system_tests/run_integration.sh
+++ system_tests/run_integration.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+# Check if BUILD_NUMBER is set, if not lets assume its local
+if [ -z ${BUILD_NUMBER+x} ]; then export BUILD_NUMBER="local"; else echo "BUILD_NUMBER is set to '$BUILD_NUMBER'"; fi
+
 echo "Get into system_tests directory"
 cd "$(dirname "$0")"
 
