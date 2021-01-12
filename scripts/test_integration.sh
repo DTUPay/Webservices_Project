2622236323975dc09eac5e0d7ad4fcfa8e9cad16
Rubatharisan Thirumathyam <ruth@netcompany.com>
2021-01-12 22:49:37 +0100

    Set CI name of docker-compose project

--- scripts/test_integration.sh
+++ scripts/test_integration.sh
@@ -5,7 +5,7 @@ set -e
 if [ -z ${BUILD+x} ]; then export BUILD="build-local"; else echo "BUILD is set to '$BUILD'"; fi
 
 echo "Starting CI environment"
-docker-compose -f docker-compose.ci.yaml up -d 
+docker-compose --project-name continous-integration -f docker-compose.ci.yaml up -d 
 
 echo "Sleep 6 seconds before running tests on CI environment"
 sleep 6
