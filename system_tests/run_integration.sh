c958275497ca36a006b7a7e57324fe362ab1a439
Rubatharisan Thirumathyam <Rubatharisan@gmail.com>
2021-01-12 11:22:06 +0100

    Update run_integration.sh

--- system_tests/run_integration.sh
+++ system_tests/run_integration.sh
@@ -1,20 +1,9 @@
 #!/bin/Bash
 
-cat << EOF 
-$$$$$$$\  $$$$$$$$\ $$\   $$\       $$$$$$$\   $$$$$$\  $$\     $$\ 
-$$  __$$\ \__$$  __|$$ |  $$ |      $$  __$$\ $$  __$$\ \$$\   $$  |
-$$ |  $$ |   $$ |   $$ |  $$ |      $$ |  $$ |$$ /  $$ | \$$\ $$  / 
-$$ |  $$ |   $$ |   $$ |  $$ |      $$$$$$$  |$$$$$$$$ |  \$$$$  /  
-$$ |  $$ |   $$ |   $$ |  $$ |      $$  ____/ $$  __$$ |   \$$  /   
-$$ |  $$ |   $$ |   $$ |  $$ |      $$ |      $$ |  $$ |    $$ |    
-$$$$$$$  |   $$ |   \$$$$$$  |      $$ |      $$ |  $$ |    $$ |    
-\_______/    \__|    \______/       \__|      \__|  \__|    \__|    
-EOF
-
 echo "Get into system_tests directory"
 cd "$(dirname "$0")"
 
 echo "Building and running integration tests!"
 cat docker-compose.yaml
 docker-compose pull
-docker-compose up
+docker-compose up --abort-on-container-exit
