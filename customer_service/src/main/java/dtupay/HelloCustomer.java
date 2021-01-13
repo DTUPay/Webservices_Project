66b29074033f670cb727f52e30c9121e94def599
Oliver O. Nielsen <olivernielsen@msn.com>
2021-01-13 09:53:46 +0100

    Rest interface added

--- customer_service/src/main/java/dtupay/HelloCustomer.java
+++ customer_service/src/main/java/dtupay/HelloCustomer.java
@@ -1,17 +1,31 @@
 package dtupay;
 
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
 
 @Path("/customer_service")
 public class HelloCustomer {
-    @GET
+
+    @POST
+    @Path("/Payment/{id}/")
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
+    public String acceptPayment(@PathParam("id") int paymentId) {
+        return "Welcome to Customer Service!";
+    }
+
+    @PUT
+    @Path("/Payment/{id}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String requestRefund(@PathParam("id") int paymentId) {
+        return "Welcome to Customer Service!";
+    }
+
+    @POST
+    @Path("/Tokens")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String requestTokens(int tokenAmount) {
         return "Welcome to Customer Service!";
     }
 }
---
+++ customer_service/src/main/java/models/PaymentRequest.java
@@ -0,0 +1,22 @@
+package dtupay;
+
+public class PaymentRequest {
+    private int MerchantId;
+    private int Amount;
+
+    public int getMerchantId() {
+        return MerchantId;
+    }
+
+    public void setMerchantId(int merchantId) {
+        MerchantId = merchantId;
+    }
+
+    public int getAmount() {
+        return Amount;
+    }
+
+    public void setAmount(int amount) {
+        Amount = amount;
+    }
+}
