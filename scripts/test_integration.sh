b7eb207a457bfc99e36187f2389c18adb1f2d8a1
Mikkel Anderson <s184230@student.dtu.dk>
2021-01-13 09:37:04 +0100

    Updated BUILD to BUILD_NUMBER

--- scripts/test_integration.sh
+++ scripts/test_integration.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 
-# Check if BUILD is set, if not lets assume its local
-if [ -z ${BUILD+x} ]; then export BUILD="build-local"; else echo "BUILD is set to '$BUILD'"; fi
+# Check if BUILD_NUMBER is set, if not lets assume its local
+if [ -z ${BUILD_NUMBER+x} ]; then export BUILD="build-local"; else echo "BUILD is set to '$BUILD'"; fi
 
 echo "Starting CI environment"
-docker-compose --project-name continous-integration -f docker-compose.ci.yaml up -d 
+docker-compose --project-name continous-integration -f docker-compose.ci.yaml up -d
 
 echo "Sleep 8 seconds before running tests on CI environment"
 sleep 8
