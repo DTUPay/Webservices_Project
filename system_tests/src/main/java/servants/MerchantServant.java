d7eaf5398c4f0fcd4f24a791f2d8b7ff3132b841
Laura <s184234@student.dtu.dk>
2021-01-13 12:57:11 +0100

    Added author message

--- system_tests/src/main/java/servants/MerchantServant.java
+++ system_tests/src/main/java/servants/MerchantServant.java
@@ -1,5 +1,8 @@
 package servants;
 
+/**
+ * @author Laura Hansen s184234
+ */
 public class MerchantServant {
     private String id;
 
@@ -11,7 +14,7 @@ public class MerchantServant {
         RestCommunicator communicator = new RestCommunicator(RestCommunicator.Service.MERCHANT.port);
         String path = RestCommunicator.Service.MERCHANT.port + "/Payment";
         // paymentrequest object m. merchant ID and payment amount
-        String url = communicator.post(amount, path);
+//        String url = communicator.post(amount, path);
     }
 
     public void generateReport() {
