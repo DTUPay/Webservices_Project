b97d642fed3d186ed4da9cfecadef86043ab4d32
Rubatharisan Thirumathyam <Rubatharisan@gmail.com>
2021-01-13 10:45:13 +0100

    Update HelloToken.java

--- token_service/src/main/java/dtupay/HelloToken.java
+++ token_service/src/main/java/dtupay/HelloToken.java
@@ -12,6 +12,6 @@ public class HelloToken {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Welcome to Token Service!";
+        return "Welcome to Token Service! Made with Love!";
     }
 }
