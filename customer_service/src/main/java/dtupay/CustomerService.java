410a531e3e693dce11298ddb84c98f3414baa012
Rubatharisan Thirumathyam <ruth@netcompany.com>
2021-01-15 20:24:21 +0100

    Added PaymentDTO

--- customer_service/src/main/java/dtupay/CustomerService.java
+++ customer_service/src/main/java/dtupay/CustomerService.java
@@ -6,15 +6,38 @@ package dtupay;
 
 
 import brokers.CustomerBroker;
+import dto.PaymentDTO;
+import dto.TokensDTO;
 import exceptions.CustomerException;
+import io.quarkus.runtime.Quarkus;
+import io.quarkus.runtime.annotations.QuarkusMain;
+import models.Callback;
 import models.Customer;
+import models.Message;
 
+import javax.json.JsonObject;
+import javax.ws.rs.container.AsyncResponse;
+import javax.ws.rs.core.Response;
+import java.util.UUID;
+
+@QuarkusMain
 public class CustomerService {
-    ICustomerRepository customerRepository;
     CustomerBroker broker;
+    ICustomerRepository customerRepository = new CustomerRepository();
+    private static CustomerService instance = new CustomerService();
+    RestResponseHandler RestfulHandler = RestResponseHandler.getInstance();
+
     public CustomerService() {
         broker = new CustomerBroker(this);
+    }
 
+    public static CustomerService getInstance(){
+        return instance;
+    }
+
+    public static void main(String[] args) {
+        CustomerService service = CustomerService.getInstance();
+        Quarkus.run();
     }
 
     public void registerCustomer(Customer customer) throws CustomerException {
@@ -43,4 +66,50 @@ public class CustomerService {
         return customerRepository.hasCustomer(cprNumber);
     }
 
+    // @TODO: Missing in UML
+    // @Status: Implemented
+    public void requestRefund(PaymentDTO payment, AsyncResponse response){
+        UUID requestId = UUID.randomUUID();
+
+        Message message = new Message();
+        message.setEvent("requestRefund");
+        message.setService("payment_service");
+        message.setRequestId(requestId);
+        message.setPayload(payment);
+        message.setCallback(new Callback("customer_service", "requestRefundResponse"));
+
+        this.broker.sendMessage(message);
+        RestfulHandler.saveRestResponseObject(response);
     }
+
+    // @TODO: Missing in UML
+    // @Status: Implemented
+    public void requestRefundResponse(Message message, JsonObject payload){
+        AsyncResponse response = RestfulHandler.getRestResponseObject(message.getRequestId());
+        response.resume(Response.status(message.getStatus()).entity(message.getStatusMessage()));
+    }
+
+    // @TODO: Missing in UML
+    // @Status: implemented
+    public void requestTokens(TokensDTO token, AsyncResponse response) {
+        UUID requestId = UUID.randomUUID();
+
+        Message message = new Message();
+        message.setEvent("requestTokens");
+        message.setService("token_service");
+        message.setRequestId(requestId);
+        message.setPayload(token);
+        message.setCallback(new Callback("customer_service", "requestTokensResponse"));
+
+        this.broker.sendMessage(message);
+        RestfulHandler.saveRestResponseObject(response);
+    }
+
+    // @TODO: Missing in UML
+    // @Status: Implemented
+    public void requestTokensResponse(Message message, JsonObject payload){
+        AsyncResponse response = RestfulHandler.getRestResponseObject(message.getRequestId());
+        response.resume(Response.status(message.getStatus()).entity(message.getStatusMessage()));
+    }
+
+}
