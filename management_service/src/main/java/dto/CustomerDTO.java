b7e709cc109984670b8771522663b82ceba10d91
Rubatharisan Thirumathyam <ruth@netcompany.com>
2021-01-16 00:45:10 +0100

    Added CustomerDTO

---
+++ management_service/src/main/java/dto/CustomerDTO.java
@@ -0,0 +1,33 @@
+package dto;
+
+import models.Payload;
+
+public class CustomerDTO extends Payload {
+    private String firstName;
+    private String lastName;
+    private String cpr;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCpr() {
+        return cpr;
+    }
+
+    public void setCpr(String cpr) {
+        this.cpr = cpr;
+    }
+}
