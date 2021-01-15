7a73b1d78a7e29c37d392f290d4b3a77db9ae9f9
Rubatharisan Thirumathyam <ruth@netcompany.com>
2021-01-15 17:44:07 +0100

    HelloManagement is now ManagementAPI

--- management_service/src/main/java/dtupay/ManagementAPI.java
+++ management_service/src/main/java/dtupay/ManagementAPI.java
@@ -11,7 +11,7 @@ import javax.ws.rs.core.MediaType;
 
 
 @Path("/management_service")
-public class HelloManagement {
+public class ManagementAPI {
     ManagementService service = new ManagementService();
 
     @GET
@@ -21,34 +21,34 @@ public class HelloManagement {
         }
 
     @POST
-    @Path("/Customer")
+    @Path("/customer")
     @Consumes(MediaType.APPLICATION_JSON)
     public String addCustomer() {
         return "A customer was added";
     }
 
     @DELETE
-    @Path("/Customer/{id}")
+    @Path("/customer/{id}")
     public String deleteCustomer(@PathParam("id") int customerId) {
         return "A customer was added";
     }
 
     @POST
-    @Path("/Merchant")
+    @Path("/merchant")
     @Produces(MediaType.APPLICATION_JSON)
     public String addMerchant(Merchant merchant) {
         return "A merchant was added";
     }
 
     @DELETE
-    @Path("/Customer/{id}")
+    @Path("/customer/{id}")
     @Consumes(MediaType.APPLICATION_JSON)
     public String deleteMerchant(@PathParam("id") int merchantId) {
         return "A customer was added";
     }
 
     @GET
-    @Path("/Report")
+    @Path("/report")
     @Produces(MediaType.APPLICATION_JSON)
     public String getReport() {
         return "Here is your advanced report";
